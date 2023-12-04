e1a13b322084aed29a2ff0b23c476ec71f08e4a1
Jason Hoo <huxueqing@126.com>
2023-12-04 11:01:50 +0800

    chore: init electron make

--- electron/main.js
+++ electron/main.js
@@ -1,42 +1,43 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron')
+
 const IS_DEV = process.env.NODE_ENV === 'development'
 
 if (process.env.NODE_ENV === 'development') {
-    // Additional development settings
-    console.log('Running in development mode');
-} else {
-    // Additional production settings
-    console.log('Running in production mode');
+  // Additional development settings
+  console.log('Running in development mode')
+}
+else {
+  // Additional production settings
+  console.log('Running in production mode')
 }
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
 
-    win.loadURL('http://localhost:5173/vitepress-unocss-starter/'); // Assuming VitePress is running on the default port
+  // win.loadURL('http://localhost:5173/vitepress-unocss-starter/') // Assuming VitePress is running on the default port
+  win.loadURL('https://www.baidu.com') // Assuming VitePress is running on the default port
 
-    win.webContents.openDevTools(); // Open DevTools - optional
+  win.webContents.openDevTools() // Open DevTools - optional
 
-    win.on('closed', () => {
-        app.quit();
-    });
+  win.on('closed', () => {
+    app.quit()
+  })
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+  if (process.platform !== 'darwin')
+    app.quit()
+})
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+  if (BrowserWindow.getAllWindows().length === 0)
+    createWindow()
+})
